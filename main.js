b3de70b26fc5f7a6e21f6091ec290d0b7466bd5d
Emma Bjørg <emso0005@stud.kea.dk>
2024-01-30 13:52:24 +0100

    nye farver

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ setInterval(() => {
     bubble.style.transform = `translate(${Math.random() * 100}vw, ${
       Math.random() * 100
     }vh)`;
-    bubble.style.background = `hsl(${Math.random() * 360}, 20%, 50%)`;
+    bubble.style.background = `hsl(${Math.random() * 360}, 50%, 50%)`;
   });
 }, 2000);
